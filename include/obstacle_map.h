c5fe1e2c372670bcd76ebf5f403c9475351975b6
jiaanlv <541281082@qq.com>
2026-07-14 19:58:12 +0800

    header

--- include/obstacle_map.h
+++ include/obstacle_map.h
@@ -1,3 +1,27 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <vector>
+#include <utility>
+
+class GridMap
+{
+    private:
+    Eigen::MatrixXi grid_;
+    int row_ = 0;
+    int cols_ = 0;
+
+    public:
+    GridMap() =default;
+    //X：动态大小；i：int
+    explicit GridMap(const Eigen::MatrixXi& grid);
+    
+    //判断坐标(x,y)是否可通行
+    bool isfree(int x, int y) const;
+    int rows() const;
+    int cols() const;
+
+    void print(const std::vector<std::pair<int, int>>& path = {} ) const;
+};
+
+GridMap createSampleMap();
